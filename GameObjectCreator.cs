5e0a8416391223723ce6f56048ba58cf18572dbe
Ashley Davis <ashley.davis@realseriousgames.com>
2015-02-25 16:18:35 +1000

    Added attribute for Unity singleton creation. Added wrapper for creating game objects.

---
+++ GameObjectCreator.cs
@@ -0,0 +1,173 @@
+﻿using RSG.Factory;
+using RSG.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Core.Unity
+{
+    /// <summary>
+    /// Interface for procedural creation and management of game objects.
+    /// All procedural game objects are automatically cleaned up on shutdown.
+    /// </summary>
+    public interface IGameObjectCreator
+    {
+        /// <summary>
+        /// Instantiate one game object from another.
+        /// Game object is parented under the 'procedural game object parent'.
+        /// </summary>
+        GameObject Instantiate(string name, GameObject source);
+
+        /// <summary>
+        /// Instantiate one game object from another under a particular parent.
+        /// </summary>
+        GameObject Instantiate(string name, GameObject source, GameObject parent);
+
+        /// <summary>
+        /// Create a named object under a particular parent
+        /// </summary>
+        GameObject Create(string name, GameObject parent);
+
+        /// <summary>
+        /// Create a named object.
+        /// Game object is parented under the 'procedural game object parent'.
+        /// </summary>
+        GameObject Create(string name);
+
+        /// <summary>
+        /// Create a permanent game object that can survive 'scene reload'.
+        /// Game object is parented under the 'procedural game object parent'.
+        /// </summary>
+        GameObject CreatePermanent(string name);
+    }
+
+    /// <summary>
+    /// Interface for procedural creation and management of game objects.
+    /// All procedural game objects are automatically cleaned up on shutdown.
+    /// </summary>
+    [Singleton(typeof(GameObjectCreator))]
+    public class GameObjectCreator : IGameObjectCreator, IStartable
+    {
+        private static readonly string ProceduralGameObjectsRootName = "_Procedural";
+
+        private static readonly string PermanentProceduralGameObjectsRootName = "_PermanentProcedural";
+
+        /// <summary>
+        /// Instantiate one game object from another.
+        /// </summary>
+        public GameObject Instantiate(string name, GameObject source)
+        {
+            Argument.StringNotNullOrEmpty(() => name);
+            Argument.NotNull(() => source);
+
+            return Instantiate(name, source, GetProceduralRootObject());
+        }
+
+        /// <summary>
+        /// Instantiate one game object from another under a particular parent.
+        /// </summary>
+        public GameObject Instantiate(string name, GameObject source, GameObject parent)
+        {
+            Argument.StringNotNullOrEmpty(() => name);
+            Argument.NotNull(() => source);
+            Argument.NotNull(() => parent);
+
+            var gameObject = (GameObject)GameObject.Instantiate(source);
+            gameObject.transform.parent = parent.transform;
+            return gameObject;
+        }
+
+        /// <summary>
+        /// Create a named object under a particular parent
+        /// </summary>
+        public GameObject Create(string name, GameObject parent)
+        {
+            Argument.StringNotNullOrEmpty(() => name);
+            Argument.NotNull(() => parent);
+
+            var gameObject = new GameObject(name);
+            gameObject.transform.parent = parent.transform;
+            return gameObject;
+        }
+
+        /// <summary>
+        /// Create a named object.
+        /// </summary>
+        public GameObject Create(string name)
+        {
+            Argument.StringNotNullOrEmpty(() => name);
+
+            return Create(name, GetProceduralRootObject());
+        }
+
+        /// <summary>
+        /// Helper function to find/create a procedural root object.
+        /// </summary>
+        private GameObject GetProceduralRootObject()
+        {
+            var parent = GameObject.Find(ProceduralGameObjectsRootName);
+            if (parent == null)
+            {
+                parent = new GameObject(ProceduralGameObjectsRootName);
+            }
+            return parent;
+        }
+
+        /// <summary>
+        /// Create a permanent game object that can survive 'scene reload'.
+        /// Game object is parented under the 'procedural game object parent'.
+        /// </summary>
+        public GameObject CreatePermanent(string name)
+        {
+            Argument.StringNotNullOrEmpty(() => name);
+
+            return CreatePermanentGameObject(name);
+        }
+
+        /// <summary>
+        /// Global function for creating a permanent game object, used to create Unity singletons.
+        /// </summary>
+        public static GameObject CreatePermanentGameObject(string name)
+        {
+            var parent = GetPermanentProceduralRootObject();
+            var gameObject = new GameObject(name);
+            gameObject.transform.parent = parent.transform;
+            gameObject.hideFlags = HideFlags.DontSave;
+            GameObject.DontDestroyOnLoad(gameObject);
+            return gameObject;
+        }
+
+        /// <summary>
+        /// Helper function to find/create a procedural root object.
+        /// </summary>
+        public static GameObject GetPermanentProceduralRootObject()
+        {
+            var parent = GameObject.Find(PermanentProceduralGameObjectsRootName);
+            if (parent == null)
+            {
+                parent = new GameObject(PermanentProceduralGameObjectsRootName);
+                parent.hideFlags = HideFlags.DontSave;
+                GameObject.DontDestroyOnLoad(parent);
+            }
+            return parent;
+        }
+
+        public void Start()
+        {
+        }
+
+        /// <summary>
+        /// Shutdown and destroy all permanent procedural game objects.
+        /// </summary>
+        public void Shutdown()
+        {
+            var parent = GameObject.Find(PermanentProceduralGameObjectsRootName);
+            if (parent != null)
+            {
+                GameObject.DestroyImmediate(parent);
+            }
+        }
+    }
+}
---
+++ UnitySingletonAttribute.cs
@@ -0,0 +1,54 @@
+﻿using RSG.Factory;
+using RSG.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Core.Unity
+{
+    /// <summary>
+    /// Attribute the defines a 'Unity Singleton', a MonoBehavior that is procedurally added to the scene and setup for dependency injection.
+    /// </summary>
+    public class UnitySingletonAttribute : SingletonAttribute
+    {
+        /// <summary>
+        /// Name of the single permanent object in the scene that contains all Unity singletons.
+        /// </summary>
+        public static readonly string UnitySingletonsGameObjectName = "_UnitySingletons";
+
+        public UnitySingletonAttribute()
+        {
+        }
+
+        public UnitySingletonAttribute(Type interfaceType) :
+            base(interfaceType)
+        {
+        }
+
+        public override object CreateInstance(IFactory factory, Type type)
+        {
+            Argument.NotNull(() => factory);
+            Argument.NotNull(() => type);
+
+            var unitySingletonHolder = GameObject.Find(UnitySingletonsGameObjectName);
+            if (unitySingletonHolder == null)
+            {
+                unitySingletonHolder = GameObjectCreator.CreatePermanentGameObject(UnitySingletonsGameObjectName);
+            }
+
+            // Instantiate the component.
+            var singletonComponent = (MonoBehaviour)unitySingletonHolder.AddComponent(type);
+            if (singletonComponent == null)
+            {
+                throw new ApplicationException(string.Format("Failed to add Unity singleton component {0} to GameObject {1}", type.Name, unitySingletonHolder.name));
+                //todo: throw new FormattedException("Failed to add Unity singleton component {SingletonTypeName} to GameObject {GameObjectName}", type.Name, unitySingletonHolder.name);
+            }
+
+            factory.ResolveDependencies(singletonComponent);
+
+            return singletonComponent;
+        }
+    }
+}
