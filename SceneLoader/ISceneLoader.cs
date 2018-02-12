06bb04e7f9f8efbfcd076a401266f042faf89110
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-02-12 11:29:43 +1000

    Split scene loader into multiple files, reject promise when scene unloading as needed

---
+++ SceneLoader/ISceneLoader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace RSG
+{
+    /// <summary>
+    /// Interface Unity scene loading.
+    /// </summary>
+    public interface ISceneLoader
+    {
+        /// <summary>
+        /// The name of the level currently scene.
+        /// </summary>
+        string CurrentSceneName { get; }
+
+        /// <summary>
+        /// Returns true when currently loading a scene.
+        /// </summary>
+        bool IsLoading { get; }
+
+        /// <summary>
+        /// Returns true when currently loading a scene.
+        /// </summary>
+        bool IsUnloading { get; }
+
+        /// <summary>
+        /// The name of the scene that is being loaded.
+        /// </summary>
+        string LoadingSceneName { get; }
+
+        /// <summary>
+        /// The name of the scene that is being loaded.
+        /// </summary>
+        string UnloadingSceneName { get; }
+
+        /// <summary>
+        /// Load a scene synchronously.
+        /// </summary>
+        void Load(string sceneName);
+
+        /// <summary>
+        /// Load a scene asynchronously.
+        /// </summary>
+        IPromise LoadAsync(string sceneName);
+
+        /// <summary>
+        /// Load a scene asynchronously.
+        /// </summary>
+        IPromise UnloadAsync(string sceneName);
+
+        /// <summary>
+        /// Load a scene asynchronously and merge it to the current scene.
+        /// </summary>
+        IPromise LoadAsyncAdditive(string sceneName);
+
+        /// <summary>
+        /// Resolves the promise when the current scene has completed.
+        /// </summary>
+        IPromise WaitUntilCurrentSceneCompleted();
+
+        /// <summary>
+        /// Notify that the current scene has complete. 
+        /// </summary>
+        void NotifyCurrentSceneCompleted();
+
+        /// <summary>
+        /// Event raised when a new scene has started async loading.
+        /// </summary>
+        event EventHandler<SceneLoadEventArgs> SceneLoading;
+
+        /// <summary>
+        /// Event raised when a new scene is loaded.
+        /// </summary>
+        event EventHandler<SceneLoadEventArgs> SceneLoaded;
+
+        /// <summary>
+        /// Event raised when a new scene has started async loading.
+        /// </summary>
+        event EventHandler<SceneUnloadEventArgs> SceneUnloading;
+
+        /// <summary>
+        /// Event raised when a new scene is loaded.
+        /// </summary>
+        event EventHandler<SceneUnloadEventArgs> SceneUnloaded;
+    }
+}
---
+++ SceneLoader/SceneUnloadEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using RSG.Utils;
+using System;
+
+namespace RSG
+{
+    /// <summary>
+    /// Args for the SceneUnloaded event.
+    /// </summary>
+    public class SceneUnloadEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The name of the scene that was loaded.
+        /// </summary>
+        public string SceneName { get; private set; }
+
+        public SceneUnloadEventArgs(string sceneName)
+        {
+            Argument.StringNotNullOrEmpty(() => sceneName);
+
+            this.SceneName = sceneName;
+        }
+    }
+}
